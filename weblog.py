f5f4669fda5dc87c4c37e3c8e9da111956eecc8f
shivam7197 <36850615+shivam7197@users.noreply.github.com>
2018-08-05 09:48:28 +0530

    Add files via upload

--- weblog.py
+++ weblog.py
@@ -1,36 +1,41 @@
 
 
-def _parse_weblog_file_forips(filename):				#Function to Parse the weblog.txt file and return different Ip_Addresses 
+def _parse_weblog_file_forips(filename):                #Function to Parse the weblog.txt file and return different Ip_Addresses 
     
     with open(filename,"r") as f:
-		
-		lines = f.readlines()  
-		ips = []
-		for line in lines:
-		    tokens = line.split(' ')
-		    ips.append(tokens[0])
-		return ips    
+        
+        lines = f.readlines()  
+        
+        all_data = []
+        for line in lines:
+            tokens = line.split(' ')
+            data = (tokens[0],tokens[6],tokens[8],tokens[9])
+            all_data.append(data)
+    return all_data    
 
 
-def get_unique_ips(filename):						#Function To Return Unique IP addresses in weblog.txt
+def get_unique_ips(filename):                        #Function To Return Unique IP addresses in weblog.txt
     
     ips = _parse_weblog_file_forips(filename)
-    unique_ips = set(ips)
+    unique_ips = []
     
+    all_ips = [idx[0] for idx in ips]
+    unique_ips = set(all_ips)
     return unique_ips      
      
     
-def get_ips_with_count(filename):		           #Function to return the dictionary of Occurences of IP_Addresses in the text file
-    
+def get_ips_with_count(filename):                   #Function to return the dictionary of Occurences of IP_Addresses in the text file
+
     ips = _parse_weblog_file_forips(filename)
     ips_dict = {}
-    for key in ips:
+    all_ips = [idx[0] for idx in ips]
+    for key in all_ips:
         ips_dict[key] = ips_dict.setdefault(key,0) + 1
             
     return ips_dict
 
 
-def get_min_occuring_ips(filename):					#Function that returns list of  IP_Addresses that are accessed MIN times
+def get_min_occuring_ips(filename):                    #Function that returns list of  IP_Addresses that are accessed MIN times
     
     ips = _parse_weblog_file_forips(filename)
     ips_min_ocurring = []
@@ -45,7 +50,7 @@ def get_min_occuring_ips(filename):					#Function that returns list of  IP_Addre
     return ips_min_ocurring
 
 
-def get_max_occuring_ips(filename):					#Function that returns list of  IP_Addresses that are accessed MAX times 
+def get_max_occuring_ips(filename):                    #Function that returns list of  IP_Addresses that are accessed MAX times 
     
     ips = _parse_weblog_file_forips(filename)
     ips_max_occuring = []
@@ -64,16 +69,20 @@ if __name__ == "__main__":
 
      filename = "weblog.txt"
      
-     unique_ips = get_unique_ips(filename)				#function call to get unique Ip_Addresses 
+     
+     data = _parse_weblog_file_forips(filename)
+     #print data
+     
+     unique_ips = get_unique_ips(filename)                #function call to get unique Ip_Addresses 
      print "\nUnique Ip Addresses In The Log File Are :: \n ",unique_ips   
      
-     ips_dict = get_ips_with_count(filename)				#function call to get Ip_Addresses with number of times they occured
+     ips_dict = get_ips_with_count(filename)                #function call to get Ip_Addresses with number of times they occured
      print "\n\nEach Ip_Address With Its Number Of Occurence in Log File :: \n",ips_dict 
      
-     ips_max_occuring = get_max_occuring_ips(filename)			#function call to get Ip Addresses that occured max number of times 
+     ips_max_occuring = get_max_occuring_ips(filename)            #function call to get Ip Addresses that occured max number of times 
      print "\nIp Addresses That Occurs Maximum Number of Times :: \n",ips_max_occuring
      
-     ips_min_occuring = get_min_occuring_ips(filename)			#function call to get Ip Addresses that occured min number of times
+     ips_min_occuring = get_min_occuring_ips(filename)            #function call to get Ip Addresses that occured min number of times
      print "\nIp Addresses That Occurs Minimum Number of Times :: \n",ips_min_occuring
       
       
